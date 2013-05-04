2cb43f4e6ee1dc0317b42a91380189af71f152f4
Ross Kinney <r2624kel@comcast.net>
2013-05-04 11:05:48 -0600

    wip

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,7 +1,19 @@
-require 'debugger'
-
-class Fibonacci 
+class Fibonacci
    
-  
+  def fibonacci(n)
+    if n < 2
+        n
+    else
+        fibonacci(n-1) + fibonacci(n-2)
+    end
+  end
+ 
+
+  # handle larger fibonacci
+  def seed_data
+    1.upto(500) do |n|
+      fibonacci(n)
+    end
+  end
 
 end
--- spec/lib/fib_spec.rb
+++ spec/lib/fib_spec.rb
@@ -1,13 +1,29 @@
+    
 require './lib/fib'
-
+ 
 describe Fibonacci do
+ 
+it "should be a Fibonacci class" do
+    subject.should be_a(Fibonacci)
+  end
+ 
+ it { subject.fibonacci(0).should == 0 }
+ it  { subject.fibonacci(1).should == (1) }
+it  { subject.fibonacci(2).should == (1) }
+it  { subject.fibonacci(3).should == (2) }
+it  { subject.fibonacci(4).should == (3) }
+it  { subject.fibonacci(5).should == (5) }
+it  { subject.fibonacci(6).should == (8) }
+it  { subject.fibonacci(7).should == (13) }
+it  { subject.fibonacci(8).should == (21) }
+it  { subject.fibonacci(9).should == (34) }
+it  { subject.fibonacci(10).should == (55) }
+it  { subject.fibonacci(12).should == (144) }
+it  { subject.fibonacci(20).should == (6765) }
+it  { subject.fibonacci(40).should == (6765) }
+
+
+
 
 
  end  
- 
-       
-
-    
-  
-
-  
